f5bf6cf9d82de8dddf0b45a75d419c3be590fa63
perty <per@artcomputer.se>
2024-02-23 07:16:33 +0100

    Fix bug when counting lines.

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -4,7 +4,7 @@ grand_total=0
 count_lines() {
     file_extension=$1
 
-    total_lines=$(find . -name "*.${file_extension}" -exec wc -l {} + | awk '{sum += $1} END {print sum}')
+    total_lines=$(find . -name "*.${file_extension}" -exec wc -l {} + | grep -vE '^[[:space:]0-9[:space:]]*total$' | awk '{sum += $1} END {print sum}')
 
     echo "Lines: ${file_extension} files: ${total_lines}"
     grand_total=$((grand_total + total_lines))
