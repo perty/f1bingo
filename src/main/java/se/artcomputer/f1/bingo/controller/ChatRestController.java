97fc77d84164dac38419e26638ee3bbb19c7d37b
Per Lundholm <per@artcomputer.se>
2024-03-11 21:03:04 +0100

    Load chat messages when page loads.

--- src/main/java/se/artcomputer/f1/bingo/controller/ChatRestController.java
+++ src/main/java/se/artcomputer/f1/bingo/controller/ChatRestController.java
@@ -1,8 +1,5 @@
 package se.artcomputer.f1.bingo.controller;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
