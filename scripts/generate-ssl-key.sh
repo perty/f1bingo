a4b1122c7dc3038671eca7b7ffdcefde52bb8900
perty <per@artcomputer.se>
2024-03-17 15:28:07 +0100

    Hint for generating key

--- scripts/generate-ssl-key.sh
+++ scripts/generate-ssl-key.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Use password found in application-dev.properties mysecret
+
 keytool -genkeypair -alias springboot -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore springboot.p12 -validity 3650
 
 mv springboot.p12 ../src/main/resources/springboot.p12
