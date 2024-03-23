a221c5a69199dd077967a181d81c32612f91d77d
perty <per@artcomputer.se>
2024-03-23 16:05:14 +0100

    Some other findings. Did not manage to log out.

--- src/main/java/se/artcomputer/f1/bingo/SecurityConfig.java
+++ src/main/java/se/artcomputer/f1/bingo/SecurityConfig.java
@@ -2,12 +2,9 @@ package se.artcomputer.f1.bingo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import se.artcomputer.f1.bingo.domain.UserDetailsServiceImpl;
 
@@ -34,18 +31,18 @@ public class SecurityConfig {
 //        return userDetailsService;
 //    }
 
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailsService);
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        return authenticationProvider;
+//    }
 
-    @Bean
-    BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- src/main/java/se/artcomputer/f1/bingo/domain/UserDetailsServiceImpl.java
+++ src/main/java/se/artcomputer/f1/bingo/domain/UserDetailsServiceImpl.java
@@ -4,14 +4,22 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserDetailsServiceImpl() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("user", "password", Collections.emptyList());
+        return new User("user", "{bcrypt}" + passwordEncoder.encode("password"), Collections.emptyList());
     }
 }
