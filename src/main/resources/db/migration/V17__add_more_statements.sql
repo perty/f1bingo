b8e94f897e6ff14f1f56bdae1767dcfdf721d576
Per Lundholm <per@artcomputer.se>
2024-03-24 08:58:36 +0100

    Add more statements.

---
+++ src/main/resources/db/migration/V17__add_more_statements.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO public.statement (id, text, race, qualifying, sprint_shootout, sprint_race)
+VALUES (126,'Efter Q1 är 10 förare inom en sekund',false,true,true,false);
+INSERT INTO public.statement (id, text, race, qualifying, sprint_shootout, sprint_race)
+VALUES (127,'Efter Q2 är 10 förare inom en sekund',false,true,true,false);
+INSERT INTO public.statement (id, text, race, qualifying, sprint_shootout, sprint_race)
+VALUES (128,'Efter Q3 är 5 förare inom en sekund',false,true,true,false);
