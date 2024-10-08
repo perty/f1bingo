a261af504f8a5cc275a31edb05e5ea3de5c0f23b
Per Lundholm <per@artcomputer.se>
2024-10-08 23:03:40 +0200

    V57: Fix the layout of grid 2025.

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v56'; // Change in misc-info.html as well.
+const CACHE_NAME = 'v57'; // Change in misc-info.html as well.
 
 self.addEventListener('install', event => {
     event.waitUntil(
