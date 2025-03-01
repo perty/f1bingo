022f64a0eb037a83b19d4a67b473f5bc42686895
Per Lundholm <per@artcomputer.se>
2025-03-01 22:41:33 +0100

    New IcalReader that handles continuation

--- src/main/java/se/artcomputer/f1/bingo/util/IcalReader.java
+++ src/main/java/se/artcomputer/f1/bingo/util/IcalReader.java
@@ -7,44 +7,51 @@ import java.util.stream.Stream;
 
 public class IcalReader {
 
-    private static String lastLocation;
-    private static String dtStart;
-    private static String dtEnd;
-    private static String summary;
-    private static long lineCounter;
+    private String location;
+    private String dtStart;
+    private String dtEnd;
+    private String summary;
+    private boolean inSummary;
 
     public static void main(String[] args) throws IOException {
-        readICalFile(args[0]);
+        new IcalReader().readICalFile(args[0]);
     }
 
-    private static void readICalFile(String file) throws IOException {
+    private void readICalFile(String file) throws IOException {
         Stream<String> lines = Files.lines(Path.of(file));
-        lines.forEach(IcalReader::processLine);
-
+        lines.forEach(this::processLine);
     }
 
-    private static void processLine(String line) {
-        lineCounter++;
+    private void processLine(String line) {
         String[] split = line.split(":");
-        String argument = split.length > 1 ? split[1].trim() : "";
+        String argument = split.length > 1 ? line.substring(line.indexOf(':') + 1) : "";
+        if (argument.isEmpty() && inSummary && line.startsWith(" ")) {
+            summary += line.substring( 1);
+        }
         switch (split[0]) {
             case "SUMMARY":
                 summary = argument;
+                inSummary = true;
                 break;
             case "LOCATION":
-                lastLocation = argument;
+                location = argument;
+                inSummary = false;
                 break;
             case "DTSTART":
                 dtStart = argument;
+                inSummary = false;
+                break;
             case "DTEND":
                 dtEnd = argument;
+                inSummary = false;
+                break;
             case "END":
                 if ("VEVENT".equals(argument)) {
-                    System.out.printf("insert into session_schedule (summary, location, starttime, endtime) values ('%s', '%s', '%s', '%s');%n", summary, lastLocation, dtStart, dtEnd);
+                    System.out.printf("insert into session_schedule (summary, location, starttime, endtime) values ('%s', '%s', '%s', '%s');%n", summary, location, dtStart, dtEnd);
                 }
                 break;
             default:
-                // Ignore
+                inSummary = false;
         }
     }
 }
