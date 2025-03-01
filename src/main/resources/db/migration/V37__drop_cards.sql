d787c930e9e3b68b3c5722591dd95153c5b381dd
Per Lundholm <per@artcomputer.se>
2025-03-01 17:55:57 +0100

    Drop old bingo cards.

---
+++ src/main/resources/db/migration/V37__drop_cards.sql
@@ -0,0 +1,3 @@
+delete from bingo_card_statement;
+delete from bingo_card;
+delete from weekend_palette;
---
+++ src/main/resources/db/migration/V38__unique_palette_per_user.sql
@@ -0,0 +1,1 @@
+create unique index unique_palette_per_user on weekend_palette (fan, weekend);
