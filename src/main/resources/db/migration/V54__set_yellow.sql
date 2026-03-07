ef032a14c030003ed500fe48bee8820509be99d7
Per Lundholm <per@artcomputer.se>
2026-03-07 09:39:39 +0100

    Set the yellow cam.

---
+++ src/main/resources/db/migration/V54__set_yellow.sql
@@ -0,0 +1,15 @@
+update driver
+set yellow_cam= true
+where code in (
+               'col', -- alpine
+               'alo', -- aston martin
+               'bor', -- audi
+               'ham', -- ferrari
+               'bot', -- cadillac
+               'bea', -- haas
+               'nor', -- mclaren
+               'ant', -- mercedes
+               'law', -- racing bulls
+               'had', -- red bull
+               'sai' -- williams
+    );
